ee4cfe7e9811aedf6aadbea21f45c1bf57c5346a
30isnottoolate <akos.varga.92@gmail.com>
2022-07-23 15:54:22 +0200

    Initialize

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div id="app">
+      <p>
+        Hello world!
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const container = ReactDOM.createRoot(document.getElementById('app-container'));
+container.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
